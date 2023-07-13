c27740df22dd8d8066aefdf487c8519af25d886c
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-13 10:52:10 +0500

    solve main save

--- get_cheapest_fruit_id.py
+++ get_cheapest_fruit_id.py
@@ -1,22 +1,21 @@
 from csv import reader
-def get_cheapest_fruit(data: str) -> str:
+def get_cheapest_fruit_id(data: str) -> int:
     """
-    This function returns the name of the cheapest fruit
+    This function returns the index of the cheapest fruit
 
     args:
-        data: CSV file with the fruits data
+        data (str): CSV file with the fruits data
     returns:
-        str: name of the cheapest fruit
+        int: id of the cheapest fruit
     """
     # your code here
     data=open('fruits.csv')
-    read=reader(data)
+    read=reader(data) 
     money=351
-    s=''
     for i in read:
         if i[1]!='price' and money>float(i[1]):
             money=float(i[1])
-            s=i[0]
-    return s
+    return int(money)
 data=str()
-print(get_cheapest_fruit(data))
+print(get_cheapest_fruit_id(data))
+    
